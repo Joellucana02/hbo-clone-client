f07506a4e34dadf6e220b5c042074822fd76959e
joel <joellucana02@gmail.com>
2021-08-21 21:02:51 -0300

    ...

--- src/pages/play.js
+++ src/pages/play.js
@@ -3,22 +3,10 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Play = () => {
-  const ROOT_API = process.env.REACT_APP_ROOT_API;
-  const TESTING_TOKEN = process.env.REACT_APP_TESTING_TOKEN;
-
   //  let [display, setDisplay] = useState([]);
   useEffect(() => {
     console.log(ROOT_API);
-    const fetchApi = async () => {
-      try {
-        const rawData = await axios.get(`${ROOT_API}/posts`, {
-          headers: { authorization: `Bearer ${TESTING_TOKEN}` },
-        });
-        const data = await rawData.data;
-        const dataArr = await data.data;
-        console.log(dataArr);
-      } catch (error) {}
-    };
+    const fetchApi = async () => {};
     fetchApi();
   }, []);
   return (
